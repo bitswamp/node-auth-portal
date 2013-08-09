978c75fb98e71ee043d091656c596ee5f79df422
Benjamin MacLeod <ben.macleod@garvin-allen.com>
2013-08-08 22:13:14 -0300

    Add router, fix connect cookie session originalUrl, refactor widget, clean up config

--- app.js
+++ app.js
@@ -1,67 +1,71 @@
-var config = require('./config.json')
+var configPath = "./config.json";
+var config = require(configPath);
 
-var proxy = require('http-proxy')
-  , passport = require('passport')
-  , connect = require('connect')
+var proxy = require('http-proxy'),
+    passport = require('passport'),
+    connect = require('connect');
+
+var injector,
+    currentUser;
 
 
-var injector = {};
+var loginWidget =
+    '<div style="[divStyle]"><a href="[target]" style="[linkStyle]" title="[linkTitle]">[linkText]</a></div>'
+    .replace("[divStyle]", config.widget.divStyle || "")
+    .replace("[target]", config.widget.target || config.loginUrl || "")
+    .replace("[linkStyle]", config.widget.linkStyle || "")
+    .replace("[linkText]", config.widget.linkText || "");
 
-var currentUser;
+injector = {};
 
+// insert the login widget at the top of the <body> tag
 injector.query = 'body';
 injector.func = function(body) {
     body.update(function(html) {
-        var loginWidget = '<div class="LoginWidget" style="'
-            + ' position: absolute; left: 0; top: 0; border-radius: 4px; padding-top: 3px;'
-            + ' height: 21px; width: 24px; background-color: rgba(180,180,180,0.5); color: gray;"'
-            + ' ><a href="' + config.loginUrl + '"' 
-            + ' style="display: block; height: 20px; width: 24px; font-family: sans-serif;'
-            + ' text-decoration: none; color: gray; text-align: center; font-weight: bold;"'
-            + ' title="' + currentUser + '">G</a>'
-            + '</div>';
-        loginWidget += html;
-        return loginWidget;
+        return loginWidget.replace("[linkTitle]", config.widget.linkTitle || currentUser || "") + html;
     });
 };
 
+
 var server = proxy.createServer(
+    function(req, res, next) {
+        // fix for connect cookie middleware expecting req.originalUrl
+        req.originalUrl = req.url;
+        next();
+    },
     connect.cookieParser(),
-    // TODO: fix unmodified cookieSession behaviour
     connect.cookieSession({ secret: config.cookieSecret, cookie: config.cookieOptions }),
-    
+
     passport.initialize(),
     passport.session(),
 
     function(req, res, next) {
         if (req.isAuthenticated()
-            && req.user 
+            && req.user
             && req.user.emails
-            && req.user.emails[0].value.indexOf(config.emailFilter) > -1) 
-        { 
+            && req.user.emails[0].value.indexOf(config.emailFilter) > -1)
+        {
             console.log(req.user.emails[0].value);
             currentUser = req.user.emails[0].value;
-            return next(); 
+            return next();
         }
 
-        // TODO: set protocol dynamically
-        req.session.redirectUrl = 'http' + '://' + config.thisHostName + req.url;
+        req.session.redirectUrl = (req.connection.encrypted ? 'https' : 'http') + '://' + req.headers.host + req.url;
+
         res.writeHead(302, {
             'Location': config.loginUrl
         });
         res.end();
     },
-    
-    // TODO: implement filter
+
     require('harmon')(null, [injector]),
-    config.targetPort,
-    config.targetHost
+    config.router
 );
 
 server.listen(config.listenPort);
-console.log("Portal server listening on " + config.listenPort);
+console.log("auth-portal listening on " + config.listenPort);
 
 
-passport.deserializeUser(function(obj, done) { 
-    done(null, obj); 
+passport.deserializeUser(function(obj, done) {
+    done(null, obj);
 });
